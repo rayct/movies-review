ec91cbc35f73c21ac834ee3bff11c37541acf997
rayct <rayct@icloud.com>
2023-02-07 03:01:44 +0000

    Movie API Endpoints

--- movies/src/main/java/dev/ray/movies/ReviewRepository.java
+++ movies/src/main/java/dev/ray/movies/ReviewRepository.java
@@ -1,8 +1,8 @@
 package dev.ray.movies;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
